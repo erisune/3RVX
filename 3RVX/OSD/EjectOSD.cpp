21573eca2d81e479ab6c226dc3b52e47f3337ad6
erisune <272665699+erisune@users.noreply.github.com>
2026-04-02 17:05:25 -0400

    Add meters in eject OSD

--- 3RVX/OSD/EjectOSD.cpp
+++ 3RVX/OSD/EjectOSD.cpp
@@ -18,8 +18,13 @@ OSD(L"3RVX-EjectDispatcher"),
 _mWnd(L"3RVX-EjectOSD", L"3RVX-EjectOSD") {
 
     SkinManager *skin = SkinManager::Instance();
-    _mWnd.BackgroundImage(skin->EjectOSD()->background);
-    _mWnd.EnableGlass(skin->EjectOSD()->mask);
+    OSDComponent *ejectOSD = skin->EjectOSD();
+    _mWnd.BackgroundImage(ejectOSD->background);
+    _mWnd.EnableGlass(ejectOSD->mask);
+    for (Meter *m : ejectOSD->meters) {
+        _mWnd.AddMeter(m);
+    }
+
     _mWnd.Update();
 
     OSD::InitMeterWnd(_mWnd);
