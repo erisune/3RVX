0f2dd459366be8e43e70ed8c301f3882b6890b13
erisune <272665699+erisune@users.noreply.github.com>
2026-04-21 00:53:14 -0400

    Add images, meters and tray icon to brightness OSD

--- 3RVX/OSD/BrightnessOSD.cpp
+++ 3RVX/OSD/BrightnessOSD.cpp
@@ -6,7 +6,9 @@
 #include "../Controllers/Brightness/BrightnessController.h"
 #include "../Controllers/Brightness/DDCBrightnessController.h"
 #include "../DisplayManager.h"
+#include "../LanguageTranslator.h"
 #include "../HotkeyInfo.h"
+#include "../MeterWnd/Meters/CallbackMeter.h"
 #include "../Skin/OSDComponent.h"
 #include "../Skin/Skin.h"
 #include "../Skin/SkinManager.h"
@@ -17,9 +19,25 @@ _mWnd(L"3RVX-BrightnessOSD", L"3RVX-BrightnessOSD") {
 
     SkinManager *skin = SkinManager::Instance();
     OSDComponent *brightnessOSD = skin->BrightnessOSD();
-    if (brightnessOSD != nullptr) {
+    if (brightnessOSD == nullptr) {
+        _validSkin = false;
+    } else {
+        _validSkin = true;
         _mWnd.BackgroundImage(brightnessOSD->background);
         _mWnd.EnableGlass(brightnessOSD->mask);
+
+        for (Meter *m : brightnessOSD->meters) {
+            _mWnd.AddMeter(m);
+        }
+
+        /* Add a callback meter with the default brightness increment */
+        _callbackMeter = new CallbackMeter(brightnessOSD->defaultUnits, *this);
+        _mWnd.AddMeter(_callbackMeter);
+
+        /* Default brightness increment */
+        _defaultIncrement = static_cast<float>(10000 / brightnessOSD->defaultUnits) / 10000.0f;
+        CLOG(L"Default brightness increment: %f", _defaultIncrement);
+
         _mWnd.Update();
     }
 
@@ -27,46 +45,263 @@ _mWnd(L"3RVX-BrightnessOSD", L"3RVX-BrightnessOSD") {
 
     _brightnessCtrl = new DDCBrightnessController(
         DisplayManager::Primary().Handle());
+
+    /* Check if brightness API is supported */
+    if (!_brightnessCtrl->SupportsBrightnessAPI()) {
+        return;
+    }
+
+    /* Set up notification icon */
+    if (_settings->BrightnessIconEnabled()) {
+        _iconImages = skin->BrightnessIconset();
+        if (_iconImages.size() > 0) {
+            _icon = new NotifyIcon(Window::Handle(), L"Brightness", _iconImages[0]);
+        }
+
+        LanguageTranslator *translator = _settings->Translator();
+        _menuDisplayStr = translator->Translate(_menuDisplayStr);
+        _menuTurnStr = translator->Translate(_menuTurnStr);
+        _menuSetStr = translator->Translate(_menuSetStr);
+        _menuExitStr = translator->Translate(_menuExitStr);
+
+        _menu = CreatePopupMenu();
+
+        InsertMenu(_menu, -1, MF_ENABLED, MENU_DISPLAY, _menuDisplayStr.c_str());
+        InsertMenu(_menu, -1, MF_SEPARATOR, NULL, NULL);
+        InsertMenu(_menu, -1, MF_ENABLED, MENU_TURNOFF, _menuTurnStr.c_str());
+        InsertMenu(_menu, -1, MF_ENABLED, MENU_SETTINGS, _menuSetStr.c_str());
+        InsertMenu(_menu, -1, MF_SEPARATOR, NULL, NULL);
+        InsertMenu(_menu, -1, MF_ENABLED, MENU_EXIT, _menuExitStr.c_str());
+
+        /* Menu accepts both left and right clicks on its items: */
+        _menuFlags = TPM_RIGHTBUTTON;
+        if (GetSystemMetrics(SM_MENUDROPALIGNMENT) != 0) {
+            _menuFlags |= TPM_RIGHTALIGN;
+        } else {
+            _menuFlags |= TPM_LEFTALIGN;
+        }
+    }
+
+    /* Enable sound effects, if any */
+    if (_validSkin) {
+        if (_settings->SoundEffectsEnabled()) {
+            _soundPlayer = brightnessOSD->sound;
+        }
+    }
+
+    UpdateBrightnessState();
 }
 
 BrightnessOSD::~BrightnessOSD() {
+    DestroyMenu(_menu);
+    delete _icon;
     delete _brightnessCtrl;
+    delete _callbackMeter;
+}
+
+void BrightnessOSD::MeterLevels(float level) {
+    if (_validSkin) {
+        _mWnd.MeterLevels(level);
+        _mWnd.Update();
+    }
+}
+
+void BrightnessOSD::MeterChangeCallback(int units) {
+    /* Callback meter changed by at least 1 unit */
 }
 
 void BrightnessOSD::Hide() {
     _mWnd.Hide(false);
 }
 
-void BrightnessOSD::HideIcon() {
+void BrightnessOSD::Show() {
+    if (OSD::Enabled() == false) {
+        return;
+    }
 
+    _mWnd.Show();
+}
+
+void BrightnessOSD::HideIcon() {
+    delete _icon;
+}
+
+void BrightnessOSD::UpdateIcon() {
+    UpdateIconImage();
+    UpdateIconTip();
+}
+
+void BrightnessOSD::UpdateIconImage() {
+    if (_icon == NULL) {
+        return;
+    }
+
+    int icon = 0;
+    size_t vUnits = _iconImages.size() - 1;
+    icon = static_cast<int>(round(_brightnessCtrl->Brightness() * vUnits));
+
+    if (icon != _lastIcon) {
+        _icon->UpdateIcon(_iconImages[icon]);
+        _lastIcon = icon;
+    }
+}
+
+void BrightnessOSD::UpdateIconTip() {
+    if (_icon == NULL) {
+        return;
+    }
+
+    float b = _brightnessCtrl->Brightness();
+    std::wstring perc = std::to_wstring(static_cast<int>(b * 100.0f));
+    std::wstring level = L"Brightness: " + perc + L"%";
+    _icon->UpdateToolTip(level);
 }
 
 void BrightnessOSD::ProcessHotkeys(HotkeyInfo &hki) {
     switch (hki.action) {
-	case HotkeyInfo::IncreaseBrightness: {
-		float newval = _brightnessCtrl->Brightness() + _stepsize;
-		if (newval > 1.0) break;
-		_brightnessCtrl->Brightness(newval);
-		}break;
-
-    case HotkeyInfo::DecreaseBrightness: {
-		float newval = _brightnessCtrl->Brightness() - _stepsize;
-		if (newval < 0.0) break;
-		_brightnessCtrl->Brightness(newval);
-        }break;
-
+	case HotkeyInfo::IncreaseBrightness:
+    case HotkeyInfo::DecreaseBrightness:
+        ProcessBrightnessHotkeys(hki);
+        break;
     case HotkeyInfo::SetBrightness:
-
+        /* I should probably rename the enum */
+        HotkeyInfo::VolumeKeyArgTypes type = HotkeyInfo::VolumeArgType(hki);
+        if (type == HotkeyInfo::VolumeKeyArgTypes::NoArgs) {
+            return;
+        } else if (type == HotkeyInfo::VolumeKeyArgTypes::Units) {
+            int numUnits = hki.ArgToInt(0);
+            _brightnessCtrl->Brightness(numUnits * _defaultIncrement);
+        } else if (type == HotkeyInfo::VolumeKeyArgTypes::Percentage) {
+            float perc = static_cast<float>(hki.ArgToDouble(0)) / 100.0f;
+            _brightnessCtrl->Brightness(perc);
+        }
+        SendMessage(Window::Handle(), BrightnessController::MSG_BRI_CHNG,
+            NULL, static_cast<LPARAM>(1));
         break;
     }
 }
 
+void BrightnessOSD::ProcessBrightnessHotkeys(HotkeyInfo &hki) {
+    float currentBrightness = _brightnessCtrl->Brightness();
+    HotkeyInfo::VolumeKeyArgTypes type = HotkeyInfo::VolumeArgType(hki);
+
+    if (type == HotkeyInfo::VolumeKeyArgTypes::Percentage) {
+        /* Deal with percentage-based amounts */
+        float amount = static_cast<float>(hki.ArgToDouble(0)) / 100.0f;
+        if (hki.action == HotkeyInfo::HotkeyActions::DecreaseBrightness) {
+            amount = -amount;
+        }
+        _brightnessCtrl->Brightness(currentBrightness + amount);
+    } else {
+        /* Unit-based amounts */
+        double unitIncrement = 1.0;
+        int currentUnit = _callbackMeter->CalcUnits();
+        if (currentBrightness <= 0.000001f) {
+            currentUnit = 0;
+        }
+
+        if (hki.action == HotkeyInfo::DecreaseBrightness) {
+            unitIncrement = -1.0;
+        }
+
+        if (type == HotkeyInfo::VolumeKeyArgTypes::Units) {
+            unitIncrement *= hki.ArgToDouble(0);
+        }
+
+        if (unitIncrement - static_cast<int>(unitIncrement) < 0.0001) {
+            _brightnessCtrl->Brightness(
+                static_cast<float>(currentUnit + unitIncrement) * _defaultIncrement);
+        } else {
+            _brightnessCtrl->Brightness(_brightnessCtrl->Brightness()
+                + (static_cast<float>(unitIncrement * _defaultIncrement)));
+        }
+    }
+
+    /* Tell 3RVX that we changed the brightness */
+    SendMessage(Window::Handle(), BrightnessController::MSG_BRI_CHNG,
+        NULL, static_cast<LPARAM>(1));
+}
+
+void BrightnessOSD::UpdateBrightnessState() {
+    float b = _brightnessCtrl->Brightness();
+    MeterLevels(b);
+    UpdateIcon();
+}
+
 void BrightnessOSD::OnDisplayChange() {
+    delete _brightnessCtrl;
+    _brightnessCtrl = new DDCBrightnessController(
+        DisplayManager::Primary().Handle());
     InitMeterWnd(_mWnd);
+}
+
+void BrightnessOSD::OnMenuEvent(WPARAM wParam) {
+    int menuItem = LOWORD(wParam);
+    switch (menuItem) {
+    case MENU_SETTINGS:
+        Settings::LaunchSettingsApp();
+        break;
+
+    case MENU_DISPLAY: {
+        CLOG(L"Menu: Display settings");
+        HINSTANCE code = ShellExecute(NULL, L"open", L"control",
+            L"desk.cpl", NULL, SW_SHOWNORMAL);
+        break;
+    }
+
+    case MENU_TURNOFF: {
+        CLOG(L"Menu: Turn off display");
+        SendMessage(_masterWnd, WM_SYSCOMMAND, SC_MONITORPOWER, static_cast<LPARAM>(2));
+        break;
+    }
+
+    case MENU_EXIT:
+        CLOG(L"Menu: Exit: %d", reinterpret_cast<int>(_masterWnd));
+        SendMessage(_masterWnd, WM_CLOSE, NULL, NULL);
+        break;
+    }
+}
+
+void BrightnessOSD::OnNotifyIconEvent(HWND hWnd, LPARAM lParam) {
+    if (lParam == WM_RBUTTONUP) {
+        POINT p;
+        GetCursorPos(&p);
+        SetForegroundWindow(hWnd);
+        TrackPopupMenuEx(_menu, _menuFlags, p.x, p.y,
+            Window::Handle(), NULL);
+        PostMessage(hWnd, WM_NULL, 0, 0);
+    }
+}
+
+void BrightnessOSD::OnBrightnessChange(HWND hWnd, WPARAM wParam, LPARAM lParam) {
+    float b = _brightnessCtrl->Brightness();
+
+    UpdateIcon();
+    MeterLevels(b);
+    Show();
+
+    if (_soundPlayer) {
+        _soundPlayer->Play();
+    }
+    HideOthers(Brightness);
 }
 
 LRESULT
 BrightnessOSD::WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    switch (message) {
+    case BrightnessController::MSG_BRI_CHNG:
+        OnBrightnessChange(hWnd, wParam, lParam);
+        break;
+
+    case MSG_NOTIFYICON:
+        OnNotifyIconEvent(hWnd, lParam);
+        break;
+
+    case WM_COMMAND:
+        OnMenuEvent(wParam);
+        break;
+    }
+
     return OSD::WndProc(hWnd, message, wParam, lParam);
 }
 
--- 3RVX/OSD/BrightnessOSD.h
+++ 3RVX/OSD/BrightnessOSD.h
@@ -3,28 +3,67 @@
 
 #pragma once
 
+#include "..\MeterWnd\MeterCallbackReceiver.h"
+#include "..\NotifyIcon.h"
 #include "OSD.h"
 
+class CallbackMeter;
 class BrightnessController;
+class SoundPlayer;
 
-class BrightnessOSD : public OSD {
+class BrightnessOSD : public OSD, MeterCallbackReceiver {
 public:
     BrightnessOSD();
     ~BrightnessOSD();
 
     void Hide();
     void HideIcon();
+    void Show();
 
     virtual void ProcessHotkeys(HotkeyInfo &hki);
 
 private:
-    MeterWnd _mWnd;
     BrightnessController *_brightnessCtrl;
+    float _defaultIncrement;
+    bool _validSkin;
+
+    MeterWnd _mWnd;
+    CallbackMeter *_callbackMeter;
+
+    NotifyIcon *_icon;
+    std::vector<HICON> _iconImages;
+    int _lastIcon;
+
+    HMENU _menu;
+    UINT _menuFlags;
+
+    SoundPlayer *_soundPlayer;
+
+    virtual void MeterChangeCallback(int units);
+    void MeterLevels(float value);
+    void ProcessBrightnessHotkeys(HotkeyInfo &hki);
+    void UpdateIcon();
+    void UpdateIconTip();
+    void UpdateIconImage();
+
+    void UpdateBrightnessState();
 
     virtual void OnDisplayChange();
+    void OnMenuEvent(WPARAM wParam);
+    void OnNotifyIconEvent(HWND hWnd, LPARAM lParam);
+    void OnBrightnessChange(HWND hWnd, WPARAM wParam, LPARAM lParam);
 
     virtual LRESULT WndProc(HWND hWnd, UINT message,
         WPARAM wParam, LPARAM lParam);
 
-	float _stepsize = 0.1f;
+private:
+    std::wstring _menuSetStr = L"Settings";
+    std::wstring _menuDisplayStr = L"Display settings";
+    std::wstring _menuTurnStr = L"Turn off display";
+    std::wstring _menuExitStr = L"Exit 3RVX";
+
+    static const int MENU_SETTINGS = 0;
+    static const int MENU_DISPLAY = 1;
+    static const int MENU_TURNOFF = 2;
+    static const int MENU_EXIT = 3;
 };
